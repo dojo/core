bfc62305280c9a6176aeaca6e5a48093b15a6582
bitpshr <paulbouchon90@gmail.com>
2015-05-01 08:57:52 -0400

    JSDoc fo Map

--- src/Map.ts
+++ src/Map.ts
@@ -1,11 +1,14 @@
 import {ArrayLike} from './array';
 import {is} from './object';
 
+/**
+ * An implementation of the ES2015 Map specification using arrays
+ * instead of iterators. The iterator protocol is not implemented.
+ */
 export class Map<K, V> {
 	private mapKeys: K[] = [];
-
 	private mapValues: V[] = [];
-	
+
 	/*
 	 * An alternative to Array.prototype.indexOf using Object.is
 	 * to check for equality. See http://mzl.la/1zuKO2V
@@ -19,11 +22,24 @@ export class Map<K, V> {
 		return -1;
 	}
 
+	/**
+	* Returns the value associated with a given key.
+	*
+	* @param key The key to look up
+	* @return the value of one exists or undefined
+	*/
 	get(key: K): V {
 		var index = this.indexOfKey(this.mapKeys, key);
 		return index < 0 ? undefined : this.mapValues[index];
 	}
 
+	/**
+	* Sets the value associated with a given key.
+	*
+	* @param key The key to define a value to
+	* @param value The value to assign
+	* @return A Map instance
+	*/
 	set(key: K, value: V): Map<K, V> {
 		var index = this.indexOfKey(this.mapKeys, key);
 		index = index < 0 ? this.mapKeys.length : index;
@@ -32,6 +48,12 @@ export class Map<K, V> {
 		return this;
 	}
 
+	/**
+	* Deletes a given key and its associated value.
+	*
+	* @param key The key to delete
+	* @return true if the key exists, false if it does not
+	*/
 	delete(key: K): boolean {
 		var index = this.indexOfKey(this.mapKeys, key);
 		if (index < 0) {
@@ -42,22 +64,47 @@ export class Map<K, V> {
 		return true;
 	}
 
+	/**
+	* Checks for the presence of a given key.
+	*
+	* @param key The key to check for
+	* @return true if the key exists, false if it does not
+	*/
 	has(key: K): boolean {
 		return this.indexOfKey(this.mapKeys, key) > -1;
 	}
 
+	/**
+	* Deletes all keys and their associated values.
+	*/
 	clear(): void {
 		this.mapKeys.length = this.mapValues.length = 0;
 	}
 
+	/**
+	* Returns an array of map keys in order of insertion.
+	*
+	* @return an array of keys in order of insertion
+	*/
 	keys(): K[] {
 		return this.mapKeys;
     }
 
+	/**
+	* Returns an array of map values in order of insertion.
+	*
+	* @return an array of values in order of insertion
+	*/
 	values(): V[] {
 		return this.mapValues;
 	}
 
+	/**
+	* Returns an array of two-value tuples in the form
+	* of [key, value] in order of insertion.
+	*
+	* @return an array of entries in order of insertion
+	*/
 	// TODO: change this any type to K, V tuple
 	entries(): any {
 		var entries: any[] = [];
@@ -67,6 +114,13 @@ export class Map<K, V> {
 		return entries;
     }
 
+	/**
+	* Executes a given function for each map entry.
+	*
+	* @param callback The function to execute for each map entry,
+	* @param context The value to use for `this` for each execution
+	* of the calbackv
+	*/
 	forEach(callback: () => any, context?: {}) {
 		// don't use this.entries to avoid second forEach call
 		this.mapKeys.forEach(function (key) {
@@ -74,6 +128,17 @@ export class Map<K, V> {
 		});
 	}
 
+	/**
+	* Creates a new Map
+	*
+	* @constructor
+	*
+	* @param iterable
+	* An array of two-item tuples used to initially
+	* populate the map. The first item in each tuple
+	* corresponds to the key of the map entry. The second
+	* item corresponds to the value of the map entry.
+	*/
 	constructor(iterable: ArrayLike<[K, V]>) {
 		if (!(this instanceof Map)) {
 			throw new TypeError('Constructor Map requires "new"');
