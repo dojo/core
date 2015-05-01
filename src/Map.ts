8bc14ed8a05d727372141ae5e55c53684e60d3ba
bitpshr <paulbouchon90@gmail.com>
2015-05-01 01:54:53 -0400

    Finish implementing Map API
    * Did not use iterators per internal discussion, just arrays
    * Constructor can be called with array-like objects as well as array
    * Still need to add documentation
    * Generics used

--- src/Map.ts
+++ src/Map.ts
@@ -2,6 +2,10 @@ import {ArrayLike} from './array';
 import {is} from './object';
 
 export class Map<K, V> {
+	private mapKeys: K[] = [];
+
+	private mapValues: V[] = [];
+	
 	/*
 	 * An alternative to Array.prototype.indexOf using Object.is
 	 * to check for equality. See http://mzl.la/1zuKO2V
@@ -15,44 +19,61 @@ export class Map<K, V> {
 		return -1;
 	}
 
-	keys: K[] = [];
-	values: V[] = [];
+	get(key: K): V {
+		var index = this.indexOfKey(this.mapKeys, key);
+		return index < 0 ? undefined : this.mapValues[index];
+	}
 
 	set(key: K, value: V): Map<K, V> {
-		var index = this.indexOfKey(this.keys, key);
-		index = index < 0 ? this.keys.length : index;
-		this.keys[index] = key;
-		this.values[index] = value;
+		var index = this.indexOfKey(this.mapKeys, key);
+		index = index < 0 ? this.mapKeys.length : index;
+		this.mapKeys[index] = key;
+		this.mapValues[index] = value;
 		return this;
 	}
 
-	clear(): void {
-		this.keys.length = this.values.length = 0;
-	}
-
 	delete(key: K): boolean {
-		var index = this.indexOfKey(this.keys, key);
+		var index = this.indexOfKey(this.mapKeys, key);
 		if (index < 0) {
 			return false;
 		}
-		this.keys.splice(index, 1);
-		this.values.splice(index, 1);
+		this.mapKeys.splice(index, 1);
+		this.mapValues.splice(index, 1);
 		return true;
 	}
 
-	entries(): any[] {
+	has(key: K): boolean {
+		return this.indexOfKey(this.mapKeys, key) > -1;
+	}
+
+	clear(): void {
+		this.mapKeys.length = this.mapValues.length = 0;
+	}
+
+	keys(): K[] {
+		return this.mapKeys;
+    }
+
+	values(): V[] {
+		return this.mapValues;
+	}
+
+	// TODO: change this any type to K, V tuple
+	entries(): any {
 		var entries: any[] = [];
-		this.keys.forEach(function (key) {
+		this.mapKeys.forEach(function (key: K) {
 			entries.push([key, this.get(key)]);
 		}, this);
 		return entries;
     }
 
 	forEach(callback: () => any, context?: {}) {
-		
+		// don't use this.entries to avoid second forEach call
+		this.mapKeys.forEach(function (key) {
+			callback.call(context, [key, this.get(key)])
+		});
 	}
 
-	// Can we type this stronger?
 	constructor(iterable: ArrayLike<[K, V]>) {
 		if (!(this instanceof Map)) {
 			throw new TypeError('Constructor Map requires "new"');
@@ -60,7 +81,7 @@ export class Map<K, V> {
 
 		if (iterable) {
 			// Can't depend on iterable protocol yet. Don't use
-			// forEach so this works with ArrayLike objects too.
+			// forEach so this works with array like objects too.
 			for (let i = 0; i < iterable.length; i++) {
 				this.set(iterable[i][0], iterable[i][1]);
 			}
