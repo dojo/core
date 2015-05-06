492a2a068c6998ffc5f3281f9289c09267ee9b5a
bitpshr <paulbouchon90@gmail.com>
2015-05-06 13:03:31 -0400

    Update emit signature to match proposal

--- src/on.ts
+++ src/on.ts
@@ -22,6 +22,12 @@ interface Evented {
 	on(event: ExtensionEvent, listener: EventListener): Handle;
 }
 
+interface EventObject {
+    type: string;
+	bubbles?: boolean;
+	cancelable?: boolean;
+}
+
 export default function on(target: EventTarget, type: string, listener: EventListener): Handle;
 export default function on(target: EventTarget, type: ExtensionEvent, listener: EventListener): Handle;
 export default function on(target: EventEmitter, type: string, listener: EventListener): Handle;
@@ -29,6 +35,10 @@ export default function on(target: EventEmitter, type: ExtensionEvent, listener:
 export default function on(target: Evented, type: string, listener: EventListener): Handle;
 export default function on(target: Evented, type: ExtensionEvent, listener: EventListener): Handle;
 export default function on(target: any, type: any, listener: EventListener): Handle {
+	if (type.call) {
+		return type.call(this, target, listener, false);
+	}
+
 	if (target.addEventListener && target.removeEventListener) {
 		target.addEventListener(type, listener, false);
 		return createHandle(function () { target.removeEventListener(type, listener, false); });
@@ -45,21 +55,22 @@ export default function on(target: any, type: any, listener: EventListener): Han
 	}
 };
 
-export function emit(target: EventTarget, type: string, event?: Object): boolean;
-export function emit(target: EventEmitter, type: string, event?: Object): boolean;
-export function emit(target: Evented, type: string, event?: Object): boolean;
-export function emit(target: any, type: string, event?: any): boolean {
+export function emit(target: EventTarget, event: EventObject): boolean;
+export function emit(target: EventEmitter, event: EventObject): boolean;
+export function emit(target: Evented, event: EventObject): boolean;
+
+export function emit(target: any, event: EventObject): boolean {
 	if (typeof target.emit === 'function' && !target.nodeType) {
-		return target.emit(type, event);
+		return target.emit(event);
 	}
 
 	if (target.dispatchEvent && target.ownerDocument && target.ownerDocument.createEvent) {
 		var nativeEvent = target.ownerDocument.createEvent('HTMLEvents');
-		nativeEvent.initEvent(type, Boolean(event.bubbles), Boolean(event.cancelable));
+		nativeEvent.initEvent(event.type, Boolean(event.bubbles), Boolean(event.cancelable));
 
 		for (var key in event) {
 			if (!(key in nativeEvent)) {
-				nativeEvent[key] = event[key];
+				nativeEvent[key];
 			}
 		}
 
