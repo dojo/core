cf220369445076693b9ac04bc834b3488b425d2b
bitpshr <paulbouchon90@gmail.com>
2015-05-13 12:26:48 -0400

    on module cleanup

--- src/on.ts
+++ src/on.ts
@@ -21,14 +21,15 @@ export interface EventEmitter {
 
 export default function on(target: EventTarget, type: string, listener: EventListener, capture?: boolean): Handle;
 export default function on(target: EventTarget, type: ExtensionEvent, listener: EventListener, capture?: boolean): Handle;
-export default function on(target: EventTarget, type: [string|ExtensionEvent], listener: EventListener, capture?: boolean): Handle;
+export default function on(target: EventTarget, type: (string | ExtensionEvent)[], listener: EventListener, capture?: boolean): Handle;
 
 export default function on(target: EventEmitter, type: string, listener: EventCallback): Handle;
 export default function on(target: EventEmitter, type: ExtensionEvent, listener: EventCallback): Handle;
+export default function on(target: EventEmitter, type: (string | ExtensionEvent)[], listener: EventCallback): Handle;
 
 export default function on(target: Evented, type: string, listener: EventCallback): Handle;
 export default function on(target: Evented, type: ExtensionEvent, listener: EventCallback): Handle;
-export default function on(target: Evented, type: [string|ExtensionEvent], listener: EventCallback): Handle;
+export default function on(target: Evented, type: (string | ExtensionEvent)[], listener: EventCallback): Handle;
 
 export default function on(target: any, type: any, listener: any, capture?: boolean): Handle {
 	if (type.call) {
@@ -50,17 +51,18 @@ export default function on(target: any, type: any, listener: any, capture?: bool
 		target.addEventListener(type, callback, capture);
 		return createHandle(function () { target.removeEventListener(type, callback, capture); });
 	}
-	else if (target.on && target.removeListener) {
+
+	if (target.on && target.removeListener) {
 		target.on(type, listener);
 		return createHandle(function () { target.removeListener(type, listener); });
 	}
-	else if (target.on) {
+
+	if (target.on) {
 		return target.on(type, listener);
 	}
-	else {
-		throw new TypeError('Unknown event emitter object');
-	}
-};
+
+	throw new TypeError('Unknown event emitter object');
+}
 
 // TODO: What should this return type signature be? Boolean for cancel? The event object itself?
 export function emit(target: EventTarget, event: EventObject): boolean;
@@ -68,28 +70,25 @@ export function emit(target: EventEmitter, event: EventObject): boolean;
 export function emit(target: Evented, event: EventObject): boolean;
 
 export function emit(target: any, event: any): boolean {
-	if (target.emit && target.removeListener) {
-		return target.emit(event.type, event);
-	}
-	else if (target.emit) {
-		return target.emit(event, event);
-	}
-
-	if (typeof target.emit === 'function' && !target.nodeType) {
-		return target.emit(event.type, event);
-	}
-
 	if (target.dispatchEvent && target.ownerDocument && target.ownerDocument.createEvent) {
 		var nativeEvent = target.ownerDocument.createEvent('HTMLEvents');
 		nativeEvent.initEvent(event.type, Boolean(event.bubbles), Boolean(event.cancelable));
 
 		for (var key in event) {
 			if (!(key in nativeEvent)) {
-				nativeEvent[key];
+				nativeEvent[key] = event[key];
 			}
 		}
 
 		return target.dispatchEvent(nativeEvent);
+	}
+	
+	if (target.emit && target.removeListener) {
+		return target.emit(event.type, event);
+	}
+
+	if (target.emit && target.on) {
+		return target.emit(event);
 	}
 
 	throw new Error('Target must be an event emitter');
