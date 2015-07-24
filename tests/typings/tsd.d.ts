323dd6563c73b7113c22a00da7299e62b185228c
Bradley Maier <bmaier@sitepen.com>
2015-07-24 17:14:11 -0400

    Remove unnecessary tsd.d.ts file.

--- tests/typings/tsd.d.ts
+++
@@ -1,6 +0,0 @@
-/// <reference path="intern/intern.d.ts" />
-/// <reference path="benchmark/benchmark.d.ts" />
-/// <reference path="sinon/sinon.d.ts" />
-/// <reference path="formidable/formidable.d.ts" />
-/// <reference path="http-proxy/http-proxy.d.ts" />
-/// <reference path="services/echo.d.ts" />
