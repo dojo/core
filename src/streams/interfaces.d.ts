d25f4aeceb07f57c3c4cd483d0c51ad30a645d84
Anthony Gubler <agubler@sitepen.com>
2016-07-19 00:24:07 +0100

    migrate streams/SeekableStreamReader.

--- src/streams/interfaces.d.ts
+++ src/streams/interfaces.d.ts
@@ -2,7 +2,7 @@ export interface Strategy<T> {
 	/**
 	 * Computes the number of items in a chunk.
 	 */
-	size?: (chunk: T) => number;
+	size?: (chunk: T | undefined) => number;
 
 	/**
 	 * The number of chunks allowed in the queue before backpressure is applied.
