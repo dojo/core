3ff0954a596482832b69e007ced01dcc4808afbc
Kitson Kelly <me@kitsonkelly.com>
2016-08-24 09:49:59 +0100

    Remove iPhone due to Intern/SauceLabs issues

--- tests/intern-saucelabs.ts
+++ tests/intern-saucelabs.ts
@@ -12,8 +12,8 @@ export const environments = [
 	{ browserName: 'firefox', version: '43', platform: 'Windows 10' },
 	{ browserName: 'chrome', platform: 'Windows 10' },
 	// { browserName: 'safari', version: '9', platform: 'OS X 10.11' },
-	{ browserName: 'android', platform: 'Linux', version: '4.4', deviceName: 'Google Nexus 7 HD Emulator' },
-	{ browserName: 'iphone', version: '9.1', deviceName: 'iPhone 6' }
+	{ browserName: 'android', platform: 'Linux', version: '4.4', deviceName: 'Google Nexus 7 HD Emulator' }// ,
+	// { browserName: 'iphone', version: '9.1', deviceName: 'iPhone 6' }
 ];
 
 /* SauceLabs supports more max concurrency */
