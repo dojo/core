f858c56b2391c53144c6a04b692a2d35ed33aaad
dylans <dylan@dojotoolkit.org>
2016-10-26 08:02:25 -0700

    improve request/util destructuring

--- src/request/util.ts
+++ src/request/util.ts
@@ -8,12 +8,12 @@ import UrlSearchParams from '../UrlSearchParams';
  * @param options The options hash that is used to generate the query string.
  */
 export function generateRequestUrl(url: string,
-        { cacheBust }: RequestOptions = {}): string {
-    let query = new UrlSearchParams(options.query).toString();
-    if (cacheBust) {
-        const bustString = String(Date.now());
-        query += query ? `&${bustString}` : bustString;
-    }
-    const separator = url.indexOf('?') > -1 ? '&' : '?';
-    return query ? `${url}${separator}${query}` : url;
+		{ cacheBust }: RequestOptions = {}): string {
+	let query = new UrlSearchParams(options.query).toString();
+	if (cacheBust) {
+		const bustString = String(Date.now());
+		query += query ? `&${bustString}` : bustString;
+	}
+	const separator = url.indexOf('?') > -1 ? '&' : '?';
+	return query ? `${url}${separator}${query}` : url;
 }
