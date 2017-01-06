3f54f9d166f4d7786946ca94998aa723d36876b1
Liang Zhou <lzhoucs@gmail.com>
2017-01-06 16:51:52 -0600

    Add unit tests for updated `load()` behaviors.

--- tests/support/load/a.ts
+++ tests/support/load/a.ts
@@ -1,2 +1,4 @@
 export const one = 1;
 export const two = 2;
+const a = 'A';
+export default a;
--- tests/support/load/node.ts
+++ tests/support/load/node.ts
@@ -3,7 +3,8 @@ import { Require } from 'dojo-interfaces/loader';
 
 declare const require: Require;
 
-export const succeed = load(require, './a', './b');
+export const succeedDefault = load(require, './a', './b');
+export const succeedNonDefault = load(require, false, './a', './b');
 export const fail = load(require, './a', './c');
 
 export const globalSucceed = load('fs', 'path');
