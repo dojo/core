2697427a63859a074332f48da567d2aed4d0ffda
Anthony Gubler <agubler@sitepen.com>
2017-05-22 13:38:25 +0100

    temp comment out safari and iPhone 9.1 from test envs

--- tests/intern.ts
+++ tests/intern.ts
@@ -42,9 +42,11 @@ export const environments = [
 	{ browserName: 'internet explorer', version: '11' },
 	{ browserName: 'edge' },
 	{ browserName: 'firefox', platform: 'WINDOWS' },
-	{ browserName: 'chrome', platform: 'WINDOWS' },
-	{ browserName: 'safari', version: '9.1', platform: 'MAC' },
-	{ browserName: 'iPhone', version: '9.1' }
+	{ browserName: 'chrome', platform: 'WINDOWS' }
+	// Issue with iphone 9.1 and Safari compatability, commented out for move to
+	// browserstack and issue raised - https://github.com/dojo/core/issues/338
+	// { browserName: 'safari', version: '9.1', platform: 'MAC' },
+	// { browserName: 'iPhone', version: '9.1' }
 ];
 
 // Maximum number of simultaneous integration tests that should be executed on the remote WebDriver service
