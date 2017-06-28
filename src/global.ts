8a831e2031c675ba366f4769b8cf9ba43a29a0a2
Kitson Kelly <me@kitsonkelly.com>
2017-06-28 18:33:58 +0100

    Re-export shim/global and change internal references

--- src/global.ts
+++ src/global.ts
@@ -1,17 +1,2 @@
-const globalObject: any = (function (): any {
-	if (typeof window !== 'undefined') {
-		// Browsers
-		return window;
-	}
-	else if (typeof global !== 'undefined') {
-		// Node
-		return global;
-	}
-	else if (typeof self !== 'undefined') {
-		// Web workers
-		return self;
-	}
-	return {};
-})();
-
+import globalObject from '@dojo/shim/global';
 export default globalObject;
